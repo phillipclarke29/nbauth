0bc86aacd4edb2460c2ec8e2d87ab57bfaf00290
Philip Clarke <philipclarke@Philips-MacBook-Pro.local>
2022-02-16 18:34:07 +0000

    minor change to trigger a build

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ function App() {
           formType === 'signedIn' && (
               <div>
               <h1> Welcome to NBFire </h1>
-                <h2>Automate your notebooks</h2>
+                <h2>Automate your notebooks here</h2>
                 <button onClick={
                   ()=> Auth.signOut()
                 }>Sign Out</button>
